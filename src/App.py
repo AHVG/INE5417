2e9dbd703b409bf70ca06a90b31f5d1bee9edfc0
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-04-02 23:35:04 -0300

    i dont know if i need delete logic for first task

--- src/App.py
+++ src/App.py
@@ -3,44 +3,27 @@ import tkinter as tk
 from Constants import SIZE_OF_BOARD
 from UltimateTicTacToe import UltimateTicTacToe
 from TKLayoutBuilder import TKLayoutBuilder
-from PlayManager import PlayManager
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.ultimate_ttt = UltimateTicTacToe()
-        self.play_manager = PlayManager(self.ultimate_ttt)
+        self.current_player = "O"
 
         self.title("Ultimate Tic Tac Toe")
-        self.initialize_board()
-
-    def initialize_board(self):
         TKLayoutBuilder(self).build(self.ultimate_ttt, self.on_click)
 
-    def on_click(self, u_position, ttt_position, button):
+    def switch_player(self):
+        self.current_player = "X" if self.current_player == "O" else "O"
 
-        if not self.play_manager.play_allowed(u_position, ttt_position):
-            return
+    def on_click(self, u_position, ttt_position, button):
         
         i, j = u_position
         k, h = ttt_position
 
         ttt = self.ultimate_ttt.childs[i][j]
-        ttt.childs[k][h].set_value(self.play_manager.current_player)
-        button.config(text=self.play_manager.current_player)
+        ttt.childs[k][h].set_value(self.current_player)
+        button.config(text=self.current_player)
 
-        if ttt.check():
-            if self.ultimate_ttt.check():
-                print(f"Player {self.play_manager.current_player} won")
-
-        window = button.master.master.master
-
-        for frame in window.winfo_children():
-            frame.config(bg="white")
-
-        next_frame = window.winfo_children()[k * SIZE_OF_BOARD + h]
-        next_frame.config(bg="gray")
-
-        self.play_manager.set_last_play(ttt_position)
-        self.play_manager.switch_player()
+        self.switch_player()
